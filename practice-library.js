b650803963bcd5014c3802614a5a75629e458fa0
fedallxn <faithe.dall@gmail.com>
2025-11-14 08:35:32 -0600

    Created a LibraryItem base class with 3 parameters and 2 methods. Extended the base class with 3 child classes: Book, Magazine, and DVD. Each had 2 extra parameters each, but I chose not to add any additional methods. The isAvailable key is defaulted to true unless stated otherwise. Instatiated a new book and magazine and performed the checkout mothod on both. This will display a message that says the book has been checked out and it must be returned in 2 weeks. Also created an array of movies that have their availability set to false. Performed the forEach function on the array of movies with the returnItem method in order to return each DVD. Another message with display saying that each title has been returned!

--- practice-library.js
+++ practice-library.js
@@ -1,1 +1,63 @@
+/*Task 1: Create Base Class*/
+
+class LibraryItem {
+    constructor(title, id, isAvailable = true) {
+        this.title = title;
+        this.id = id;
+        this.isAvailable = isAvailable;
+    }
+    checkOut() {
+        this.isAvailable = false;
+        console.log(`${this.title} has been checked out. Please return in 2 weeks.`)
+    }
+    returnItem() {
+        this.isAvailable = true;
+        console.log(`${this.title} has been returned. Thank you!`)
+    }
+}
+
+/*Task 2: Extend the Base Class*/
+
+class Book extends LibraryItem {
+    constructor(title, id, author, genre, isAvailable = true) {
+        super(title, id, isAvailable = true)
+        this.author = author;
+        this.genre = genre;
+    }
+}
+
+class DVD extends LibraryItem {
+    constructor(title, id, director, duration, isAvailable = true) {
+        super(title, id, isAvailable = true)
+        this.director = director;
+        this.duration = duration;
+    }
+}
+
+class Magazine extends LibraryItem {
+    constructor(title, id, issueNumber, publisher, isAvailable = true) {
+        super(title, id, isAvailable = true)
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
+}
+
+/*Task 3: Instantiate Objects*/
+
+let classic = new Book("Project Hail Mary", 213496, "Andy Weir", "Science-Fiction");
+let gamerMag = new Magazine("PC Gamer", 38308, 416, "Future Publishing");
+let movies = [
+    new DVD("FernGully: The Last Rainforest", 458766, "Bill Kroyer", "76 minutes", false),
+    new DVD("Hook", 458797, "Steven Spielberg", "142 minutes", false),
+    new DVD("Treasure Planet", 458845, "John Musker", "95 minutes", false)
+];
+
+/*Task 4: Test the Inheritance*/
+console.log([classic, gamerMag]);
+classic.checkOut();
+gamerMag.checkOut();
+console.log([classic, gamerMag]);
+console.log(movies);
+movies.forEach(movie => {movie.returnItem()});
+
 
